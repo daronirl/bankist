6c783f6e569418420bd68af1411e28b7404728e4
John Prescott <48197513+daronirl@users.noreply.github.com>
2022-12-26 16:11:24 -0800

    finished tab components with adding and removing classes

--- app.js
+++ app.js
@@ -136,9 +136,19 @@ const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
-  console.log(clicked);
+  
 
   // Guard clause
   if (!clicked) return; 
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate Tab
   clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+  
 });
